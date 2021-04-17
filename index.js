eed8067a1ccca462067800d73757d14b03b01db2
javimarashall <jamondragon@ucdavis.edu>
2021-04-17 11:26:08 -0700

    questions

--- index.js
+++ index.js
@@ -4,34 +4,51 @@ const employeeQuestions = [
     {
         type: "input",
         name: "name",
-        message: "What is your employee name?",
+        message: "What is your employee's name?",
     },
     {
         type: "input",
         name: "id",
-        message: "What is your employee ID?",
+        message: "What is your employee's ID?",
     },
     {
         type: "input",
         name: "email",
-        message: "What is your employee e-mail?",
+        message: "What is your employee's e-mail?",
     },
 ]
 const engineerQuestions = [
     {
         type: "input",
         name: "name",
-        message: "What is your engineer name?",
+        message: "What is your engineer's name?",
     },
     {
         type: "input",
         name: "id",
-        message: "What is your engineer ID?",
+        message: "What is your engineer's ID?",
     },
     {
         type: "input",
         name: "email",
-        message: "What is your engineer e-mail?",
+        message: "What is your engineer's e-mail?",
+    },
+]
+const managerQuestions = [
+    {
+        type: "input",
+        name: "name",
+        message: "What is your name?",
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "What is your ID?",
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your e-mail?",
     },
     {
         type = "input",
@@ -39,11 +56,28 @@ const engineerQuestions = [
         message = "What is your office number?",
     },
 ]
+const internQuestions = [
+    {
+        type: "input",
+        name: "name",
+        message: "What is your intern's name?",
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "What is your intern's ID?",
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your intern's e-mail?",
+    },
+]
 
 function getEmployee(){
     inquirer.prompt(questions)
     .then(data => {
         const employee = new Employee (data.name, data.id, data.email)
-        team.push(employee);
+        //team.push(employee);
     })
 }
