ed03d6d137388cea6d1fbc8d2f536f4bca034459
javimarashall <jamondragon@ucdavis.edu>
2021-04-17 13:15:11 -0700

    employee test

---
+++ tests/employee.test.js
@@ -0,0 +1,32 @@
+const Employee = require("../lib/employee")
+
+describe("Employee class", () =>{
+    describe("getEmployeeName", () => {
+        it("Get employee name", () => {
+            const name = "javier";
+            const employee = new Employee(name, "1", "jamondra@ucdavis.edu", "javimarashall");
+            expect(employee.getEmployeeName().ToBe("javier"));
+})
+});
+    describe("getID", () => {
+        it("Get employee id", () => {
+            const ID = "10";
+            const employee = new Employee("javier", ID, "jamondra@ucdavis.edu", "javimarashall");
+            expect(employee.getID().ToBe("10"));
+        })
+});
+    describe("getEmail", () => {
+        it("Get employee e-mail", () => {
+            const email = "jamondragon@ucdavis.edu";
+            const employee = new Employee("javier", "10", email, "javimarashall");
+            expect(employee.getEmail().ToBe("jamondragon@ucdavis.edu"));
+    })
+});
+describe("getGitHub", () => {
+    it("Get employee GitHub", () => {
+        const gitHub = "javimarashall";
+        const employee = new Employee("javier", ID, "jamondra@ucdavis.edu", gitHub);
+        expect(employee.getGitHub().ToBe("javimarashall"));
+    })
+});
+})
---
+++ tests/engineer.test.js
@@ -0,0 +1,32 @@
+const Engineer = require("../lib/engineer")
+
+describe("Engineer class", () =>{
+    describe("getEngineerName", () => {
+        it("Get engineer name", () => {
+            const name = "javier";
+            const engineer = new Engineer(name, "1", "jamondra@ucdavis.edu", "javimarashall");
+            expect(engineer.getEngineerName().ToBe("javier"));
+})
+});
+    describe("getID", () => {
+        it("Get engineer id", () => {
+            const ID = "10";
+            const engineer = new Engineer("javier", ID, "jamondra@ucdavis.edu", "javimarashall");
+            expect(engineer.getID().ToBe("10"));
+        })
+});
+    describe("getEmail", () => {
+        it("Get engineer e-mail", () => {
+            const email = "jamondragon@ucdavis.edu";
+            const engineer = new Engineer("javier", "10", email, "javimarashall");
+            expect(engineer.getEmail().ToBe("jamondragon@ucdavis.edu"));
+    })
+});
+describe("getGitHub", () => {
+    it("Get engineer GitHub", () => {
+        const gitHub = "javimarashall";
+        const engineer = new Engineer("javier", ID, "jamondra@ucdavis.edu", gitHub);
+        expect(engineer.getGitHub().ToBe("javimarashall"));
+    })
+});
+})
